e307f37c893b6599d9f02572b29a92c637aab7e5
jonesfernando <90173818+jonesfernando@users.noreply.github.com>
2021-09-23 22:43:51 +0530

    Update urls.py

--- hackernews1/api/urls.py
+++ hackernews1/api/urls.py
@@ -1,12 +1,20 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
-
-router = routers.DefaultRouter()
-router.register(r'posts', views.PostViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+from django.contrib import admin
+from django.urls import path
+from .views import *
+ 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('',PostListView, name='home'),
+    path('new',NewPostListView, name='new_home'),
+    path('past',PastPostListView, name='past_home'),
+    path('user/<username>', UserInfoView, name='user_info'),
+    path('posts/<username>',UserSubmissions, name='user_posts'),
+    path('post/<int:id>',CommentListView, name='post'),
+    path('submit',SubmitPostView, name='submit'),
+    path('signin',signin, name='signin'),
+    path('signup',signup, name='signup'),
+    path('signout',signout, name='signout'),
+    path('vote/<int:id>',UpVoteView,name='vote'),
+    path('downvote/<int:id>',DownVoteView,name='dvote'),
+    path('edit/<int:id>',EditListView, name='edit'),
+    path('post/<int:id1>/comment/<int:id2>',CommentReplyView,name='reply'),
 ]
